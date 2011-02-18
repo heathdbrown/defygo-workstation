00d4716b06a11d1ed6fa1bdabe5e4c4fe90cf575
Andy H <hillad@gmail.com>
2011-02-17 20:54:43 -0600

    Added comments, replaced gedit themes with gmate.

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# Update OS
 sudo apt-get -y update 
+# Base packages for rvm, pgsql, git, and rails.
 sudo apt-get -y install chromium-browser build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev meld
+# git complete
 wget --no-check-certificate https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
 mv git-completion.bash .git-completion.sh
 echo "PS1='[\u@\h \W\$(__git_ps1 \" (%s)\")]\$ '" >> ~/.bashrc
 echo "source ~/.git-completion.sh" >> ~/.bashrc
+# RVM
 bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
 echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session." >> ~/.bashrc
 source ~/.bashrc
@@ -16,16 +20,13 @@ rvm --default use 1.9.2@defygo
 gem update --system
 gem install rails --version 3.0.4
 mkdir rails_projects
+# git configuration
 git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global merge.tool meld
 git config --global core.editor gedit
-git clone git://github.com/mig/gedit-rails.git
-cd gedit-rails
-sudo ~/.rvm/bin/rake-ruby-1.9.2-p136@defygo setup
-sudo chown -R `whoami`:`whoami` ~/.gnome2/gedit
+# gmate gedit textmate styles
+git clone git://github.com/gmate/gmate.git
+cd gmate
+sudo ./install.sh
 cd ..
-git clone git://github.com/mig/gedit-themes.git
-mkdir ~/.gnome2/gedit/styles
-cp gedit-themes/*.xml ~/.gnome2/gedit/styles
-
